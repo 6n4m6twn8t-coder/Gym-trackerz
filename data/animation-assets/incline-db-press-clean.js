73797b7632cd3de3ca3f4c484c76694fcd414801
6n4m6twn8t-coder <6n4m6twn8t@privaterelay.appleid.com>
2026-08-24 16:21:59 +1000

    Assemble clean incline press animation

---
+++ data/animation-assets/incline-db-press-clean.js
@@ -0,0 +1,6 @@
+import { c1 } from "./incline-clean-c1.js";
+import { c2 } from "./incline-clean-c2.js";
+import { c3 } from "./incline-clean-c3.js";
+import { c4 } from "./incline-clean-c4.js";
+
+export const inclineDbPressClean = "data:image/webp;base64," + c1 + c2 + c3 + c4;
