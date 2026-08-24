e29ea2deb3d9c4c8f4732b5454ca5712f4e0ce97
6n4m6twn8t-coder <6n4m6twn8t@privaterelay.appleid.com>
2026-08-24 17:03:53 +1000

    Assemble stabilized incline press v9

---
+++ data/animation-assets/incline-db-press-stable-v9.js
@@ -0,0 +1,8 @@
+import { s1 } from "./incline-stable-v9-c1.js";
+import { s2 } from "./incline-stable-v9-c2.js";
+import { s3a } from "./incline-stable-v9-c3a.js";
+import { s3b } from "./incline-stable-v9-c3b.js";
+import { s4a } from "./incline-stable-v9-c4a.js";
+import { s4b } from "./incline-stable-v9-c4b.js";
+
+export const inclineDbPressStableV9 = "data:image/webp;base64," + s1 + s2 + s3a + s3b + s4a + s4b;
