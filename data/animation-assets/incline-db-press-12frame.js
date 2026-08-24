3cf81a56247edb716b9f8755e4b921fb63884d86
6n4m6twn8t-coder <6n4m6twn8t@privaterelay.appleid.com>
2026-08-24 14:57:44 +1000

    Assemble compact 12-frame incline press demo

---
+++ data/animation-assets/incline-db-press-12frame.js
@@ -0,0 +1,4 @@
+import { p1 } from "./incline-db-press-small-p1.js";
+import { p2 } from "./incline-db-press-small-p2.js";
+
+export const inclineDbPress12Frame = "data:image/webp;base64," + p1 + p2;
